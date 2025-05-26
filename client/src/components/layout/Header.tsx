70cb5ae29aed5d5b4bc57e4681add7f1d68fa650
patrick-fullstack <patrick@fullstack.ph>
2025-05-26 10:56:13 +0800

    fixed header and footer

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -52,10 +52,10 @@ export default function Header() {
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link href={isAuthenticated ? "/dashboard" : "/"} className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-amber-700" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 116 0z" clipRule="evenodd" />
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-amber-700" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8z M9,11A1,1 0 0,1 10,12A1,1 0 0,1 9,13A1,1 0 0,1 8,12A1,1 0 0,1 9,11z M15,11A1,1 0 0,1 16,12A1,1 0 0,1 15,13A1,1 0 0,1 14,12A1,1 0 0,1 15,11z M12,7C12,7 13.33,5.57 14.67,5.57C15.99,5.57 17.35,9 12,9C6.65,9 8.01,5.57 9.33,5.57C10.67,5.57 12,7 12,7z" />
               </svg>
-              <span className="ml-2 text-xl font-bold bg-gradient-to-r from-amber-700 to-orange-600 bg-clip-text text-transparent">AuthGram</span>
+              <span className="ml-2 text-xl font-bold bg-gradient-to-r from-amber-700 to-orange-600 bg-clip-text text-transparent"></span>
             </Link>
           </div>
 
